a6a556866d4dc7bb76768a2586ae95d2145f8f9a
Nahid Hassan <41489906+0nahid@users.noreply.github.com>
2021-09-08 17:53:45 +0600

    added rechartjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
+import React, { useEffect, useState } from 'react';
+import News from './components/News/News';
 
 function App() {
+  const apiKey = 'https://newsapi.org/v2/top-headlines?country=us&?q=nasa&ln=en&apiKey=6b23a9ae371c4946a324f098d26ca7da';
+  const [articles, setArticles] = useState([]);
+  useEffect(() => {
+    fetch(apiKey)
+      .then(response => response.json())
+      .then(data => setArticles(data.articles))
+  })
   return (
-    <Button variant="contained" color="primary">
-      Hello World
-    </Button>
+    <div>
+      {articles.map(article => <News news={article} key={article.url} ></News>)}
+    </div>
   );
 }
 
